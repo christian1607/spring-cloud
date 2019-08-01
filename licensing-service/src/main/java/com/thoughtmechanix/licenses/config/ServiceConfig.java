47a078c1172e3386c6a6ba71b358b2f0e2c01de1
= <christian.altamirano.ayala@gmail.com>
2019-07-31 23:03:58 -0500

    refreas scope license-service

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 public class ServiceConfig {
 
     @Value("${example.property}")
